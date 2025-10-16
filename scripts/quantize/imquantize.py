010058020dbc1326a4390c65d977121b36f0272f
fhermosilloup <fhermosillo@up.edu.mx>
2025-10-16 15:32:57 -0600

    Update imquantize.py

--- scripts/quantize/imquantize.py
+++ scripts/quantize/imquantize.py
@@ -1,11 +1,37 @@
 import cv2
 import numpy as np
+import os
+from tkinter import Tk, filedialog, messagebox
 
 # --- CONFIGURACIÓN ---
-IN_IMG = 'img2.jpg'
-OUT_FILE = 'mem.bin'
 WIDTH, HEIGHT = 320, 240
-BITS_PER_CHANNEL = 4  # 4 bits → 16 niveles
+BITS_PER_CHANNEL = 4        # 4 bits → 16 niveles
+
+# --- 0. Seleccionar archivo ---
+root = Tk()
+root.withdraw()  # Oculta la ventana principal de Tkinter
+
+IN_IMG = filedialog.askopenfilename(
+    title="Seleccionar imagen",
+    filetypes=[("Imagenes", "*.jpg *.png *.jpeg *.bmp *.tiff")]
+)
+
+if not IN_IMG:
+    raise SystemExit("No se seleccionó ningún archivo.")
+
+# Cuadro de diálogo para guardar imagen
+OUT_FILE = filedialog.asksaveasfilename(
+    title="Guardar archivo de memoria como",
+    defaultextension=".mem",
+    filetypes=[("Memory Initialization Files", "*.mem")]
+)
+if not OUT_FILE:
+    base, _ = os.path.splitext(IN_IMG)
+    OUT_FILE = base + ".mem"
+    messagebox.showinfo(
+        "Aviso",
+        f"No se seleccionó archivo de salida.\nSe guardará como:\n{OUT_FILE}"
+    )
 
 # --- 1. Cargar y reescalar la imagen ---
 img = cv2.imread(IN_IMG)
@@ -14,11 +40,12 @@ if img is None:
 
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_resized = cv2.resize(img_rgb, (WIDTH, HEIGHT), interpolation=cv2.INTER_NEAREST)
-img2 = cv2.cvtColor(img_resized, cv2.COLOR_RGB2BGR)
 
-cv2.imshow("Original", img)
-cv2.imshow("Resized", img2)
-cv2.waitKey()
+
+#img2 = cv2.cvtColor(img_resized, cv2.COLOR_RGB2BGR)
+#cv2.imshow("Original", img)
+#cv2.imshow("Resized", img2)
+#cv2.waitKey()
 
 
 
@@ -41,4 +68,4 @@ with open(OUT_FILE, 'w') as f:
             rgb_12bit = (r*256) + (g*16) + b
             f.write(f"{rgb_12bit:03X}\n")  # 3 dígitos hex (000–FFF)
 
-print(f"Archivo '{OUT_FILE}' generado con {WIDTH*HEIGHT} palabras de 12 bits.")
+messagebox.showinfo("Proceso completado", f"Archivo {OUT_FILE} generado correctamente:\n")
