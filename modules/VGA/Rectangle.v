c659b71cff2b5e622fc9c686b5d0bccd8cbb7326
fhermosilloup <fhermosillo@up.edu.mx>
2025-11-03 13:04:13 -0600

    Add files via upload

---
+++ modules/VGA/Rectangle.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 29.10.2025 16:29:57
+// Design Name: 
+// Module Name: Rectangle
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Rectangle(
+	input wire x,
+	input wire y,
+	input wire display,
+	input wire [9:0] x0, // 0-799
+	input wire [9:0] y0, // 0-524
+	input wire [9:0] w,
+	input wire [9:0] h,
+	input wire [11:0] color,
+	output reg [11:0] rgb,
+	output reg inRange
+);
+	always @(*)
+	begin
+		if(display)
+		begin
+			if(x >= x0 && x < x0+w && y >= y0 && y < y0+h)
+			begin
+				rgb = color;
+				inRange = 1;
+			end
+			else
+			begin
+				rgb = 12'h000;
+				inRange = 0;
+			end
+		end
+	end
+endmodule
